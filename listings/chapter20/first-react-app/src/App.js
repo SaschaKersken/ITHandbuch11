755807ec697d170f7f94218f403a5a8bb6cf69c9
Sascha Kersken <saschakersken@MBP-von-Sascha.fritz.box>
2023-03-12 22:33:45 +0100

    Updated a React example

--- listings/chapter20/first-react-app/src/App.js
+++ listings/chapter20/first-react-app/src/App.js
@@ -7,33 +7,30 @@ class App extends Component {
     this.state = { clicks: 0 };
   }
 
-  /*componentDidMount() {
-    fetch("http://localhost/language/")
-      .then(result => result.text())
-      .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
-      .then(data => console.log(data));
-  }*/
-
   clickHandler(evt) {
     evt.preventDefault();
     const { clicks } = this.state;
     let increasedClicks = clicks + 1;
     this.setState( { clicks: increasedClicks } );
   }
-    
+
   render() {
     const { clicks } = this.state;
 
     return (
       <div className="App">
-        {clicks == 1 ?
-        <p>Bisher 1 Klick.</p>
-        :
+        {clicks === 0 &&
+        <p>Es wurde noch nicht geklickt.</p>}
+        {clicks === 1 &&
+        <p>Bisher 1 Klick.</p>}
+        {clicks > 1 &&
         <p>Bisher {clicks} Klicks.</p>}
-        <p><a href="#" onClick = {evt => this.clickHandler(evt)}>Noch ein Klick</a></p>
+        <p><button onClick = {evt => this.clickHandler(evt)}>Noch ein Klick</button></p>
       </div>
     );
   }
+
 }
 
 export default App;
+
